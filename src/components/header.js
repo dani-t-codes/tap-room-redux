1414b6fb27009682b9928faf0c048ea2c5209bc9
Danielle Thompson <danithompson74@gmail.com>
2021-02-12 15:56:02 -0800

    Add jumbotron styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
   <>
-    <div class="jumbotron" style={{backgroundColor: "#6d8764",}}>
+    <div class="jumbotron" style={{backgroundColor: "#6d8764", color: "white", textShadow: "4px 2px 0px #d3d3d3",}}>
       <h1 style={{textAlign: 'center', fontSize: "60px", letterSpacing: '20px'}}>THE TAP ROOM</h1>
     </div>
   </>
