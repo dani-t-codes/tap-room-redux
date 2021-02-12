6b2ad04e1cbb93c616021fe4d3c1a9a979515048
Danielle Thompson <danithompson74@gmail.com>
2021-02-12 11:28:49 -0800

    Finish Header component

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   <>
     <div class="jumbotron">
-      
+      <h1 style={{textAlign: 'center', letterSpacing: '5px'}}>The Tap Room</h1>
     </div>
   </>
 }
