aa0c02a3a7cf47d337561cabe5812a03ca616f5a
Danielle Thompson <danithompson74@gmail.com>
2021-02-12 15:32:53 -0800

    Update objectives progress

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,7 +4,12 @@ import PropTypes from "prop-types";
 function Keg(props) {
   return (
     <>
-    <div class="card" style={{backgroundColor: "#6d8764", color: "white", textShadow: "1px 1px 0px #d3d3d3", padding: '10px', margin: '10px', width: '36rem'}} onClick = { () => props.whenKegClicked(props.id)}>
+    <div class="card"
+      style={{backgroundColor: "#6d8764",
+      color: "white", textShadow: "1px 1px 0px #d3d3d3",
+      padding: '10px',
+      margin: '10px', width: '26rem'}}
+      onClick = { () => props.whenKegClicked(props.id)}>
       <h3><em>Beer:</em> {props.name}</h3>
       <h3><em>By:</em> {props.brand}</h3>
     </div>
