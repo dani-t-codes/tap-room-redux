4e9988fd53b82ef641734e555f4279a7591d3650
Danielle Thompson <danithompson74@gmail.com>
2021-02-12 11:09:58 -0800

    Update App.js component with Header and KegControl

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import KegControl from './KegControl';
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <Header />
+        <KegControl />
+      </div>
+    </>
+  );
+}
+
+export default App;
