9b565103b772da37605e4dfb3581fcc8ed878e3d
Danielle Thompson <danithompson74@gmail.com>
2021-02-12 17:29:27 -0800

    Add color coded price to details

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -9,14 +9,18 @@ function KegDetail(props) {
       <hr />
       <h3>Name: {keg.name}</h3>
       <h3>Brand or Brewery: {keg.brand}</h3>
+      { keg.priceByKeg > 90
+      ? <h3 style={{color: 'red'}}>Cost per Keg: ${keg.priceByKeg}</h3>
+      : <h3>Cost per Keg: ${keg.priceByKeg}</h3>}
       <h3>Alcohol Content: {keg.alcoholContent}%</h3>
       {keg.pintsLeft === 0 ?
       <h3>Out of stock! Better luck next time.</h3>
       : <h3>Pints Remaining: {keg.pintsLeft}</h3>}
-      {keg.pintsLeft > 0 && keg.pintsLeft <= 10 ?
-      <h4><em>Almost Empty - get 'em while they're cold!</em></h4>
+      {keg.pintsLeft > 0 && keg.pintsLeft <= 10
+      ? <h4><em>Almost Empty - get 'em while they're cold!</em></h4>
       : null}
-      { keg.pintsLeft > 0 ? <button onClick={props.onClickingBuy} class="btn btn-success">Buy Pint</button>
+      { keg.pintsLeft > 0
+      ? <button onClick={props.onClickingBuy} class="btn btn-success">Buy Pint</button>
       : <button onClick={props.onClickingRestock} class="btn btn-info">Restock Large Keg</button>}
       <button onClick={() => onClickingDelete(keg.id)} class="btn btn-danger">Delete Keg</button>
       <hr/>
