2b9cd8a4baa8ffdc0cfa2fec566ce11a8367ab0c
Danielle Thompson <danithompson74@gmail.com>
2021-02-14 17:39:49 -0800

    Add image to jumbotron

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import KegControl from './KegControl';
+import background from ".././img/jon-tyson-s2ryvZkG1Rk-unsplash.jpg";
 
 function App() {
   return (
     <>
-      <div class="container">
+      <div class="container" style={{backgroundImage: `url(${background}` }}>
         <Header />
         <KegControl />
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from "react";
 function Header() {
   return (
   <>
-    <div class="jumbotron" style={{backgroundColor: "#6d8764", color: "white", textShadow: "4px 2px 0px #d3d3d3",}}>
-      <h1 style={{textAlign: 'center', fontSize: "60px", letterSpacing: '20px'}}>THE TAP ROOM</h1>
+    <div class="jumbotron" style={{backgroundColor: "#6d8764", textShadow: "4px 2px 0px #d3d3d3",}}>
+      <h1 style={{textAlign: 'center', fontSize: "70px", letterSpacing: '30px'}}>THE TAP ROOM</h1>
     </div>
   </>
   );
