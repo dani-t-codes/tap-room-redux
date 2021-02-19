780f61c819e52f50d76ffb0767fdd50d15ffe00c
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 10:30:16 -0800

    Purpusely fail test to add new keg

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,43 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('keg list reducer', () => {
+
+  let action;
+  const kegData = {
+    name: 'Tyrannical IPA',
+    brand: 'Fort George',
+    priceByKeg: 120,
+    alcoholContent: 8.4,
+    pintsLeft: 124,
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brand, priceByKeg, alcoholContent, pintsLeft, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name,
+      brand,
+      priceByKeg,
+      alcoholContent,
+      pintsLeft,
+      id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name,
+        brand,
+        priceByKeg,
+        alcoholContent,
+        pintsLeft,
+        id
+      }
+    });
+  });
+
 });
