d3e4b09b51b86f28f54107f8b3f7dd822b4d5583
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 10:36:12 -0800

    Purposely pass test to add new keg

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, priceByKeg, alcoholContent, pintsLeft, id } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id]: {
+          name,
+          brand,
+          priceByKeg,
+          alcoholContent,
+          pintsLeft,
+          id
+        }
+      });
+    default:
+      return state;
+    }
 };
