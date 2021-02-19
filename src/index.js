99c1ad581815ff583a3080e53ee8131117c7fea1
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 12:43:23 -0800

    Add console.log for testing store.subscribe()

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import { Provider } from 'react-redux';
 
 const store = createStore(reducer);
 
+//for testing only
+store.subscribe(() => console.log(store.getState()));
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
