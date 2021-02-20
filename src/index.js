ef5c472212bf1bb1278bda257be9bb4231b242df
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 16:10:41 -0800

    Update root reducer in entry point

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { createStore } from 'redux';
-import reducer from './reducers/ticket-list-reducer';
+//import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 //for testing only
 store.subscribe(() => console.log(store.getState()));
