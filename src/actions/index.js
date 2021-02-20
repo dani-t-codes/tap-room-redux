df3032ef55e051c824671a1c3b37e1905e0b014c
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 17:04:55 -0800

    Purposely pass addKeg action creator test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,21 @@
-export const deleteKeg = id => ({
-  type: 'DELETE_KEG',
-  id
-});
+export const addKeg = (keg) => {
+  const { name, brand, priceByKeg, alcoholContent, pintsLeft, id } = keg;
+  return {
+    type: 'ADD_KEG',
+    name,
+    brand,
+    priceByKeg,
+    alcoholContent,
+    pintsLeft,
+    id
+  }
+}
 
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
+});
+
+export const deleteKeg = id => ({
+  type: 'DELETE_KEG',
+  id
 });
