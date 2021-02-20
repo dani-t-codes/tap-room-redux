b9c06fe9c4e7200af257adf26d8883111a5db964
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 16:47:14 -0800

    Purposely fail addKeg action creator test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,6 +1,18 @@
 import * as actions from './../../actions';
 
 describe('tap room actions', () => {
+  it('addKeg should create ADD_KEG action', () => {
+    expect(actions.addKeg({name: 'Tyrannical IPA', brand: 'Fort George',priceByKeg: 120, alcoholContent: 8.4, pintsLeft: 124, id: 1})).toEqual({
+      type: 'ADD_KEG',
+      name: 'Tyrannical IPA',
+      brand: 'Fort George',
+      priceByKeg: 120,
+      alcoholContent: 8.4,
+      pintsLeft: 124,
+      id: 1
+    });
+  });
+
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
       type: 'TOGGLE_FORM'
