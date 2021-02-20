6225b68885774cf3a5385d4ea4ccfdfdef952b5f
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 16:43:29 -0800

    Purposely fail toggle form action creator test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,6 +1,12 @@
 import * as actions from './../../actions';
 
 describe('tap room actions', () => {
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
       type: 'DELETE_KEG',
