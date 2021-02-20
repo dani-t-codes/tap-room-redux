c6d3b2a16408d18f4f301854804d489ff3cd33b1
Danielle Thompson <danithompson74@gmail.com>
2021-02-19 16:42:00 -0800

    Purposely pass delete keg action creator test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -3,7 +3,7 @@ import * as actions from './../../actions';
 describe('tap room actions', () => {
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
-      type: 'DELETE_TICKET',
+      type: 'DELETE_KEG',
       id: 1
     });
   });
